9407066a97ce67fcc85f954507afc1f856610854
Steve Le <le.steve@drakesoftware.com>
2026-04-23 00:37:54 +0700

    Implement useAsyncData for declarative programming, making the code more readable

--- app/components/StockDashboard.vue
+++ app/components/StockDashboard.vue
@@ -1,16 +1,31 @@
 <script setup lang="ts">
 import type { Company } from '~/models/companies';
 import type { DisplayStock } from '~/models/displayStock';
+import type { RealTimeStockPriceResponse } from '~~/shared/models/stock';
 
-const stockState = useState<DisplayStock[]>('stocks', () => []);
-const isLoading = ref(false);
 const stockList = ref(['DGW', 'MWG', 'VGC', 'SSI', 'TCX', 'VHM', 'VNM', 'FUETCC50', 'FUEVFVND', 'E1VFVN30']);
 const stockParameter = computed(() => stockList.value.join(';'));
 const stockControl = ref('');
 
-onServerPrefetch(async () => {
-  await getStocks();
-});
+const { data: stockState, status, refresh } = await useAsyncData(
+  'stocks',
+  () => {
+    if (!stockParameter.value) {
+      return Promise.resolve({} as RealTimeStockPriceResponse);
+    }
+    return useStocks().fetchStockPrice(stockParameter.value);
+  },
+  {
+    transform: (data) => Object.entries(data).map(([symbol, stockData]) => ({
+      symbol,
+      price: stockData.Price,
+      change: stockData.Price - stockData.RefPrice,
+      changePercent: stockData.RefPrice !== 0 ? ((stockData.Price - stockData.RefPrice) / stockData.RefPrice) * 100 : undefined,
+      date: useFormatDate(stockData.LastTradeDate),
+    } as DisplayStock)
+    )
+  }
+);
 
 function addStock(symbol: string) {
   if (!symbol) {
@@ -23,7 +38,7 @@ function addStock(symbol: string) {
   if (companies.value.some((company: Company) => company.Symbol === symbol.toUpperCase())) {
     if (!stockList.value.includes(symbol)) {
       stockList.value.push(symbol);
-      getStocks();
+      refresh();
     } else {
       console.warn(`Stock ${symbol} is already in the list.`);
     }
@@ -34,51 +49,28 @@ function addStock(symbol: string) {
   }
 }
 
-async function getStocks() {
-  isLoading.value = true;
-  try {
-    if (!stockParameter.value) {
-      console.warn('No stock symbols to fetch.');
-      stockState.value = [];
-      return;
-    }
-
-    const data = await useStocks().fetchStockPrice(stockParameter.value);
-    stockState.value = Object.entries(data).map(([symbol, stockData]) => ({
-      symbol,
-      price: stockData.Price,
-      change: stockData.Price - stockData.RefPrice,
-      changePercent: stockData.RefPrice !== 0 ? ((stockData.Price - stockData.RefPrice) / stockData.RefPrice) * 100 : undefined,
-      date: useFormatDate(stockData.LastTradeDate),
-    }));
-  } catch (error) {
-    console.error('Error fetching stock data:', error);
-  } finally {
-    isLoading.value = false;
-  }
-}
-
 function removeStock(symbol: string) {
   stockList.value = stockList.value.filter((s) => s !== symbol);
-  getStocks();
+  refresh();
 }
 </script>
 <template>
   <div>
     <h1>Stock Dashboard</h1>
     <div class="stocks-container">
-      <div>
-      <v-text-field v-model="stockControl" label="Stock to add"></v-text-field>
-      <v-btn @click="addStock(stockControl)" color="primary">Add Stock</v-btn>
+      <div class="d-flex">
+        <v-text-field v-model="stockControl" label="Stock to add"></v-text-field>
+        <v-btn @click="addStock(stockControl)" color="primary">Add Stock</v-btn>
       </div>
-      <div v-if="isLoading" class="text-center">
-        <v-progress-circular indeterminate color="primary" class="my-3"/>
+      <div v-if="status === 'pending'" class="text-center">
+        <v-progress-circular indeterminate color="primary" class="my-3" />
       </div>
-      <StockCard v-for="stock in stockState" :key="stock.symbol" v-bind="stock" @delete="removeStock"/>
+      <div v-if="status === 'error'">Error fetching stock data.</div>
+      <StockCard v-for="stock in stockState" :key="stock.symbol" v-bind="stock" @delete="removeStock" />
     </div>
 
     <div class="mt-5">
-      <email-subscription v-if="stockState.length > 0" />
+      <email-subscription v-if="stockState && stockState.length > 0" />
     </div>
   </div>
 </template>
